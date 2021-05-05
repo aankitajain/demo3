0ef789d76232012f46011f61a611d1bcec135295
Ankita Jain <aankitajain.2016@gmail.com>
2021-05-05 16:21:07 +0530

    commit 1

---
+++ Mavendemo/src/test/java/com/cg/project/test/AppTest.java
@@ -0,0 +1,23 @@
+package com.cg.project.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest 
+{
+	 @Test
+	    public void test1() {
+	        Assert.fail();
+	    }
+	    @Test
+	    public void test2() {
+	        Assert.fail();
+	    }
+	    @Test
+	    public void test3() {
+	       
+	    }
+	    @Test
+	    public void test4() {
+	    }
+}
